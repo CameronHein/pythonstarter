e92804a2cac186e9d7a07c7017f4e4dbd49c9956
Cameron <cameron@theboi.ttosl.org>
2014-12-05 09:59:30 -0800

    I changed this.:

--- forloop.py
+++ forloop.py
@@ -1,3 +1,6 @@
 for x in range(0, 255):
  print(x, end=' = ')
- print(chr(x))
+ print(chr(x));
+for y in range(0, 89):
+ print(y, end=' = ')
+ print(chr(y));
--- printpi.py
+++ printpi.py
@@ -1,2 +1,4 @@
 print('The value of PI is approximately {}.'.format(math.pi))
 print('The value of PI is approximately {!r}.'.format(math.pi)
+println('The value of PI is approximately {!e}.'.format(math.pi);
+
